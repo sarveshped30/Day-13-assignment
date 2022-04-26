a49050df79fed8be0b903114e66c06585f63d432
sarveshped30 <sarveshpednekar30@gmail.com>
2022-04-26 22:38:00 +0530

    Added: Day 13 assignment UC2

--- Generics.java
+++ Generics.java
@@ -1,6 +1,6 @@
-/*This package includes java program to find max problem using generics
+/* This package includes java program to find max problem using generics
 * Day 13 assignment RFP batch
-* Author: Sarvesh pednekar*/
+* Author: Sarvesh pednekar */
 
 package com.bridgelabz.generics;
 
@@ -11,38 +11,75 @@ import java.util.Scanner;
 
 public class Generics {
     static Logger logger = Logger.getLogger(Generics.class);
-    static int max;     // Declaring static max variable
+    Scanner sc = new Scanner(System.in);    //Creating Scanner object
+    static int maxInteger;     // Declaring static max variable
+    static float maxFloat;
 
     /* Creating maxInteger method for finding max value from Integer array */
     public void maxInteger(Integer[] a) {
-        // Using extended for loop for finding max integer
+        // Using extended for loop for finding max integer value
         for(int data : a) {
-            if(data > max) {        //Assigning value to max if element of array is greater than max
-                max = data;
+            if(data > maxInteger) {        //Assigning value to maxInteger if element of array is greater than maxInteger
+                maxInteger = data;
             }
         }
 
-        //Printing max value
-        logger.info("Maximum number is: " + max);
+        //Printing maxInteger value
+        logger.info("Maximum integer value is: " + maxInteger + "\n");
     }
 
-    public static void main(String[] args) {
-        BasicConfigurator.configure();
-        Generics generics = new Generics();     //Creating object of generics class
-        Scanner sc = new Scanner(System.in);    //Creating Scanner object
+    /* Creating printMaxInteger method for taking user input and printing max value */
+    public void printMaxInteger() {
+        //Taking user input for number of elements in Integer array
+        logger.info("Enter number of elements in Integer array: ");
+        int n1 = sc.nextInt();
 
-        //Taking user input for number of elements in array
-        logger.info("Enter number of elements in array: ");
-        int n = sc.nextInt();
-
-        Integer[] a = new Integer[n];       //Creating Integer object with reference variable a
+        Integer[] a = new Integer[n1];       //Creating Integer object with reference variable a
 
         //for loop for initializing elements in array
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n1; i++) {
             logger.info("Enter element in Index [" + i + "] : ");
             a[i] = sc.nextInt();
         }
 
-        generics.maxInteger(a);     //Calling maxInteger method
+        maxInteger(a);     //Calling maxInteger method
+    }
+
+    /* Creating maxFloat method for finding max value from float array */
+    public void maxFloat(Float[] b) {
+        // Using extended for loop for finding max float value
+        for(float data : b) {
+            if(data > maxFloat) {       //Assigning value to maxFloat if element in array is greater than maxFloat
+                maxFloat =  data;
+            }
+        }
+
+        //Printing maxFloat value
+        logger.info("Maximum float value is: " + maxFloat + "\n");
+    }
+
+    /* Creating printMaxIFloat method for taking user input and printing max value */
+    public void printMaxFloat() {
+        //Taking user input for number of elements in Float array
+        logger.info("Enter number of elements in Float array: ");
+        int n2 = sc.nextInt();
+
+        Float[] b = new Float[n2];       //Creating Integer object with reference variable a
+
+        //for loop for initializing elements in array
+        for (int i = 0; i < n2; i++) {
+            logger.info("Enter element in Index [" + i + "] : ");
+            b[i] = sc.nextFloat();
+        }
+
+        maxFloat(b);       //Calling maxFloat method
+    }
+    
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        Generics generics = new Generics();     //Creating object of generics class
+
+        generics.printMaxInteger();     //Calling printMaxInteger function to print max value
+        generics.printMaxFloat();       //Calling printMaxFloat function to print max value
     }
 }
